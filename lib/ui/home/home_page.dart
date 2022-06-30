df07095584ec7270e8bdb9099d4bce561a5c1f0b
RYO1223 <kuwanor0811@gmail.com>
2022-06-30 12:08:42 +0900

    refactor: ページ構成を修正

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -2,9 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:warikan/ui/home/home_state.dart';
+import 'package:warikan/data/model/group/group.dart';
 import 'package:warikan/ui/home/home_view_model.dart';
-import 'package:warikan/ui/home/payment_list.dart';
 import 'package:warikan/ui/shared/drawer.dart';
 
 class HomePage extends HookConsumerWidget {
@@ -12,56 +11,65 @@ class HomePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final router = AutoRouter.of(context);
     final state = ref.watch(homeViewModelProvider);
+    final viewModel = ref.watch(homeViewModelProvider.notifier);
 
     return state.when(
-        error: (e, msg) => Text(e.toString()),
-        loading: () {
-          return const Scaffold(
-              body:
-                  SafeArea(child: Center(child: CircularProgressIndicator())));
-        },
-        data: (data) => HomeTab(state: data));
+      error: (e, msg) => Text(e.toString()),
+      loading: () {
+        return const Scaffold(
+            body: SafeArea(child: Center(child: CircularProgressIndicator())));
+      },
+      data: (data) {
+        final tabController =
+            useTabController(initialLength: data.groups.length + 1);
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('HomePage'),
+            bottom: TabBar(
+              controller: tabController,
+              tabs: data.groups.map((group) => Tab(text: group.name)).toList()
+                ..add(const Tab(icon: Icon(Icons.add))),
+              isScrollable: true,
+            ),
+          ),
+          body: TabBarView(
+            controller: tabController,
+            children: data.groups
+                .map<Widget>((group) => PaymentList(group: group))
+                .toList()
+              ..add(const Text('')),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => viewModel.onFloatingActionButtonPressed(
+              router: router,
+              group: data.groups[tabController.index],
+            ),
+            child: const Icon(Icons.add),
+          ),
+          drawer: const MyDrawer(),
+        );
+      },
+    );
   }
 }
 
-class HomeTab extends HookConsumerWidget {
-  const HomeTab({Key? key, required this.state}) : super(key: key);
+class PaymentList extends ConsumerWidget {
+  const PaymentList({Key? key, required this.group}) : super(key: key);
 
-  final HomeState state;
+  final Group group;
 
   @override
   Widget build(context, ref) {
-    final router = AutoRouter.of(context);
-    final viewModel = ref.watch(homeViewModelProvider.notifier);
-
-    final tabController =
-        useTabController(initialLength: state.groups.length + 1);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomePage'),
-        bottom: TabBar(
-          controller: tabController,
-          tabs: state.groups.map((group) => Tab(text: group.name)).toList()
-            ..add(const Tab(icon: Icon(Icons.add))),
-          isScrollable: true,
-        ),
-      ),
-      body: TabBarView(
-        controller: tabController,
-        children: state.groups
-            .map<Widget>((group) => PaymentList(group: group))
-            .toList()
-          ..add(const Text('')),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => viewModel.onFloatingActionButtonPressed(
-          router: router,
-          group: state.groups[tabController.index],
-        ),
-        child: const Icon(Icons.add),
-      ),
-      drawer: const MyDrawer(),
+    return ListView(
+      children: group.payments.map((payment) {
+        return ListTile(
+          title: Text(payment.name),
+          subtitle: Text(payment.price.toString()),
+        );
+      }).toList(),
     );
   }
 }
